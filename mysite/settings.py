e87cfe5e375bccdf9dbff20838d475464900ae78
esmechen <esme.chen@yahoo.com>
2018-12-08 20:52:29 -0500

    Debugging static files not loading on public server when DEBUG is false.

--- mysite/settings.py
+++ mysite/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'America/New_York'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
@@ -119,5 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/essy1515/static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
